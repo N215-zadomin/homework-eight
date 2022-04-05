811269e9090245e1e38201bf743620dd95a95412
zaydee8223 <83433819+zaydee8223@users.noreply.github.com>
2022-04-05 00:25:17 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,6 @@
+#Homework Eight
+
+Link to Web4 Page :
+https://in-info-web4.informatics.iupui.edu/~zadomin/homework-eight/
+
+For this assignment we continued to practice JQuery. For this project, we had to create a navigation bar that has four links, and add content to the page when you click on a link. We were to only use one page and load all the content onto that page. Based on the instructions, all pages must have full content. This assignment was actually fairly simple and pretty fun to complete. I didn't find myself struggling all that much.
---
+++ app/app.js
@@ -0,0 +1,413 @@
+var homeContent = `  <div class="hero"></div>
+<div class="content">
+  <h1>WOMAN</h1>
+  <div class="products">
+    <div class="product">
+      <div class="prod-image1"></div>
+      <div class="prod-descrip">
+        <h4>Lyrics:</h4>
+        <p>
+          Hey, woman
+Hey, woman
+Hey, woman
+Let me be your woman
+Woman, woman, woman
+I can be your woman
+Woman, woman, woman
+Let me be your woman
+Woman, woman, woman
+I can be your woman
+Woman, woman, woman
+What you need?
+She give tenfold, come here, papa, plant your seed
+She can grow it from her womb, a family
+Provide lovin' overlooked and unappreciated, you see
+You can reciprocate
+I got delicious taste
+You need a woman's touch in your place
+Just protect her and keep her safe
+Baby, worship my hips and waist
+So feminine with grace
+I touch your soul when you hear me say </p>
+      </div>
+    </div>
+
+    <div class="product">
+      <div class="prod-descrip">
+        <p>
+          "Boy, let me be your woman"
+          Let me be your woman
+          Woman, woman, woman
+          I can be your woman
+          Woman, woman, woman
+          Let me be your woman
+          Woman, woman, woman
+          I can be your woman
+          Woman, woman, woman
+          I can be your lady, I'm a woman
+          I'm a mother****er but they got a problem
+          Put some babies in your life and take away the drama
+          Put that paper in the picture, like a diorama
+          Gotta face a lot people that are opposite
+          'Cause the world told me we ain't got that common sense
+          Gotta prove it to myself that I'm on top of shit
+          And you will never know a God, without a Goddess
+          As honest as ****in' honest get
+          And I could be on everything
+          I mean I could be the leader, head of all the states
+          I could smile and jiggle it 'til his pockets empty
+          I could be the CEO, just look at Robyn Fenty
+          And I'ma be there for you 'cause you on my team, girl
+          Don't ever think you ain't hella these niggas dream girl
+        </p>
+      </div>
+      <div class="prod-image2"></div>
+    </div>
+
+    <div class="product">
+      <iframe width="350" height="350" src="https://www.youtube.com/embed/g7X9X6TlrUo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <div class="prod-descrip">
+        <p>They wanna pit us against each other when we succeedin' for no reasons
+          They wanna see us end up like we Regina on Mean Girls
+          Princess or queen, tomboy or king (yeah)
+          You've heard a lot, you've never seen (nah)
+          Mother Earth, Mother Mary rise to the top
+          Divine feminine, I'm feminine (why?)
+          Woman (daddy)
+          Let me be your woman (let me be your)
+          Woman, woman, woman (I need to be your) (daddy)
+          I can be your woman (I know)
+          Woman, woman, woman (daddy)
+          Let me be your woman (I know)
+          Woman, woman, woman (daddy)
+          I can be your woman (I know)
+          Woman, woman, woman</p>
+      </div>
+    </div>
+  </div>
+</div>
+`;
+
+var aboutContent = `     <div class="hero"></div>
+<div class="content">
+  <h1>YOU RIGHT</h1>
+  <div class="products">
+    <div class="product">
+      <div class="prod-image3"></div>
+      <div class="prod-descrip">
+        <h4>Lyrics:</h4>
+        <p>
+          I got a man, but I want you
+          I got a man, but I want you
+          And it's just nerves, it's just ****
+          Makin' me think 'bout someone new
+          You know I got so much to say
+          I try to hide it in my face
+          And it don't work, you see through
+          That I just want get wit' you
+          And you're right
+          You right, I got my guy
+          But I, I, can't help it, I want you
+          Said, you right, I got my guy
+          But I, I, can't help it I want you
+          I can't stop and look the other way
+          'Cause I know what could be, babe
+          And you never feel the same
+          You'd be thinkin' 'bout it every day
+          Don't believe in fairytales, but we got our fantasies
+          And it's me and you, no she
+          Tryna be all through your sheets
+          Have you all on top of me
+          Acting like it's not that deep
+          Boy, you can take it out on me
+          Tell me what it's 'bout to be
+           </p>
+      </div>
+    </div>
+
+    <div class="product">
+      <div class="prod-descrip">
+        <p>
+          Really feel it's bound to be
+          I can't tell no one but they all know
+          I got a man, but I want you
+          I got a man, but I want you
+          And it's just nerves, it's just ****
+          Makin' me think 'bout someone new
+          You know I got so much to say
+          I try to hide it in my face
+          And it don't work, you see through
+          That I just want get wit' you
+          And you're right
+          You right, I got my guy (you right, you right, I)
+          But I, I, can't help it I want you
+          Said, you right, I got my guy (you right, you right, I)
+          But I, I, can't help it I want you
+          Girl, I want you like you want me too
+          I feel that energy (ooh, yeah)
+          When you're on top of me (oh, yeah)
+          I know your man, he ain't controllin' you
+          But you still hesitate (ooh, no)
+          'Cause you chose loyalty (oh, yeah)
+          And I know your history, met him before your peak (hey, hey)
+         
+        </p>
+      </div>
+      <div class="prod-image4"></div>
+    </div>
+
+    <div class="product">
+      <iframe width="350" height="350" src="https://www.youtube.com/embed/ieLLUeB-aLk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <div class="prod-descrip">
+        <p> He's so connected to that woman that you used to be
+          But-but-but this sex will cloud your memory
+          A couple strokes to put it in, then you'll belong to me
+          I got a man, but I want you
+          I got a man, but I want you (oh)
+          And it's just nerves, it's just ****
+          Makin' me think 'bout someone new
+          You know I got so much to say
+          I try to hide it in my face
+          And it don't work, you see through
+          That I just want get wit' you (yeah, yeah, yeah, yeah, yeah)
+          And you're right
+          You right, I got my guy (you right, you right, I)
+          But I, I, can't help it I want you
+          Said, you right, I got my guy (you right, you right, I)
+          But I, I, can't help it I want you</p>
+      </div>
+    </div>
+  </div>
+</div>`;
+
+var productsContent = `  <div class="hero"></div>
+<div class="content">
+  <h1>KISS ME MORE</h1>
+  <div class="products">
+    <div class="product">
+      <div class="prod-image5"></div>
+      <div class="prod-descrip">
+        <h4>Lyrics:</h4>
+        <p>
+          We hug and yes, we make love
+          And always just say "Goodnight" (la-la-la-la-la)
+          And we cuddle, sure I do love it
+          But I need your lips on mine
+          Can you kiss me more?
+          We're so young, boy
+          We ain't got nothin' to lose, oh, oh
+          It's just principle
+          Baby, hold me
+          'Cause I like the way you groove, oh, oh
+          Boy, you write your name, I can do the same
+          Ooh, I love the taste, la-la-la-la
+          All on my tongue, I want it (la-la-la-la)
+          Boy, you write your name, I can do the same
+          Ooh, I love the taste, la-la-la-la-la
+          All on my tongue, I want it
+          I, I feel like ****in' somethin'
+          But we could be corny, **** it
+          Sugar, I ain't no dummy, dummy
+          I like to say, what if, but if
+          We could kiss and just cut the rubbish
+          Then I might be on to somethin'
+          I ain't givin' you one in public
+          I'm givin' you hundreds, **** it
+         
+           </p>
+      </div>
+    </div>
+
+    <div class="product">
+      <div class="prod-descrip">
+        <p>
+          Somethin' we just gotta get into
+          Sign first, middle, last, on the wisdom tooth
+          ****** wishin' that the ***** was a kissin' booth
+          Taste breakfast, lunch and gin and juice
+          And that dinner just like dessert too
+          And when we French, refresh, gimme two
+          When I bite that lip, come get me too
+          He want lipstick, lip gloss, hickeys too, uh
+          Can you kiss me more?
+          We're so young, boy
+          We ain't got nothin' to lose, oh, oh
+          It's just principle
+          Baby, hold me
+          'Cause I like the way you groove, oh, oh
+          Boy, you write your name, I can do the same
+          Ooh, I love the taste, la-la-la-la
+          All on my tongue, I want it (la-la-la-la)
+          Boy, you write your name, I can do the same
+          Ooh, I love the taste, la-la-la-la-la
+          All on my tongue, I want it
+          Say give me a buck, need that gushy stuff
+          Push the limit, no, you ain't good enough
+          All your ****** say that you lost without me
+          All my ******* feel like I dodged the county
+          ****in' with you feel like jail, *****
+          I can't even exhale, *****
+          
+         
+        </p>
+      </div>
+      <div class="prod-image6"></div>
+    </div>
+
+    <div class="product">
+    <div class="prod-image7"></div>
+      <div class="prod-descrip">
+        <p> ***** like holy grail, you know that
+          You gon' make me need bail, you know that
+          Caught dippin' with your friend
+          You ain't even have man, lyin' on ya-, you know that
+          Got me a bag full of brick, you know that
+          Control, don't slow the pace if I throw back
+          All this *** for real
+          Drama make you feel
+          Fantasy and whip appeal
+          Is all I can give you
+          Can you kiss me more?
+          We're so young, boy
+          We ain't got nothin' to lose, oh, oh
+          It's just principle
+          Baby, hold me
+          'Cause I like the way you groove, oh, oh
+          Oh, darlin'
+          Boy, you write your name, I can do the same
+          Ooh, I love the taste, oh-la-la-la-la
+          All on my tongue, I want it
+          Boy, you write your name, I can do the same
+          Ooh, I love the taste, oh-la-la-la-la-la
+          All on my tongue, I want it</p>
+      </div>
+    </div>
+  </div>
+</div>  `;
+
+var contactContent = ` <div class="hero"></div>
+      <div class="content">
+        <h1>I DON'T DO DRUGS</h1>
+        <div class="products">
+          <div class="product">
+            <div class="prod-image8"></div>
+            <div class="prod-descrip">
+              <h4>Lyrics:</h4>
+              <p>
+                Love got me ****ed up
+                Got me on stuck, chasing that rush
+                Had to give in, couldn't give up
+                I just want you, but I don't do drugs
+                Still I want you, ooh-ooh
+                Ooh-ooh, ooh-ooh, ooh-ooh
+                Ooh-ooh, ooh-ooh, ooh-ooh
+                Ooh-ooh, ooh-ooh, ooh-ooh
+                All I want is you, ooh-ooh
+                Ooh-ooh, ooh-ooh, ooh-ooh
+                Ooh-ooh, ooh-ooh, ooh-ooh
+                Ooh-ooh, ooh-ooh, ooh-ooh
+                Baby, I've been in my bag
+                You don't really hit me back
+                'Cause I feel like I'm attached
+                Should've never kissed you back
+                Ever since I fell in love
+                Not quite like being trapped
+                More like a sickness that
+                Keeps me addicted bad
+               
+                 </p>
+            </div>
+          </div>
+      
+          <div class="product">
+            <div class="prod-descrip">
+              <p>
+                Hard for me to let you go (let you go, let you go)
+                My body wouldn't let me hide it (hide it)
+                No matter what, I wouldn't fall (wouldn't fall, wouldn't fall)
+                Fightin' through the thunder, lightning
+                Forgive me, I brought it upon mysеlf
+                (Ooh, woah, oh, oh, oh, oh)
+                Needed a fix of you, not just a kiss from you
+                I needed more
+                Lovе got me ****ed up
+                Got me on stuck, chasing that rush
+                Had to give in, couldn't give up
+                I just want you, but I don't do drugs
+                Still I want you, ooh-ooh (all I want is, my baby)
+                Ooh-ooh, ooh-ooh, ooh-ooh (all I want is, my baby)
+                Ooh-ooh, ooh-ooh, ooh-ooh (all I want is, my baby)
+                Ooh-ooh, ooh-ooh, ooh-ooh (my baby, my baby)
+                All I want is you, ooh-ooh (all I want is, my baby)
+                Ooh-ooh, ooh-ooh, ooh-ooh (all I want is, my baby)
+                Ooh-ooh, ooh-ooh, ooh-ooh (all I want is, my baby)
+                Ooh-ooh, ooh-ooh, ooh-ooh (my baby, my baby)
+                Tryna stay in my lane
+                But you play on the safe side
+                But you ****in' me up on this FaceTime
+                Faded, pourin' all over you
+               
+              </p>
+            </div>
+            <div class="prod-image9"></div>
+          </div>
+      
+          <div class="product">
+            <iframe width="350" height="350" src="https://www.youtube.com/embed/4s7xBkndEPA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <div class="prod-descrip">
+              <p> Know exactly what I wanna do
+                So, baby, can you eat it from the back?
+                Got me purrin' like I'm Doja Cat
+                Keep me in your bag
+                'Cause you wanna hear me say I'm coming back
+                But this time I want support, just make it last, ooh-woah, oh
+                It's hard for me to let you go (let you go)
+                My body wouldn't let me hide it
+                No matter what, I wouldn't fold (wouldn't fold)
+                Ridin' through the thunder, lightning
+                Forgive me, I brought it upon myself
+                (Ooh, woah, oh, oh, oh, oh)
+                Needed a fix of you, not just some kiss from you
+                I needed more
+                Love got me ****ed up
+                Got me on stuck, chasing that rush
+                Had to give in, couldn't give up (I couldn't give up)
+                I just want you, but I don't do drugs
+                Still I want you, ooh-ooh (all I want is, my baby)
+                Ooh-ooh, ooh-ooh, ooh-ooh (all I want is you)
+                Ooh-ooh, ooh-ooh, ooh-ooh (all I want is, my baby)
+                Ooh-ooh, ooh-ooh, ooh-ooh (my baby, my baby)
+                All I want is you, ooh-ooh
+                Ooh-ooh, ooh-ooh, ooh-ooh (all I really wanted)
+                Ooh-ooh, ooh-ooh, ooh-ooh (is you)
+                Ooh-ooh, ooh-ooh, ooh-ooh (yeah)
+                </p>
+            </div>
+          </div>
+        </div>
+      </div>
+   `;
+function initListeners() {
+  $("nav a").click(function (e) {
+    let btnID = e.currentTarget.id;
+    loadPageContent(btnID);
+ 
+    // console.log("click " + contentID);
+  });
+}
+
+function loadPageContent(pageName) {
+  let contentID = pageName + "Content";
+  $("#app").html(eval(contentID));
+  
+}
+
+function initApp() {
+  initListeners();
+
+}
+
+$(document).ready(function () {
+  loadPageContent("home");
+  initApp();
+});
